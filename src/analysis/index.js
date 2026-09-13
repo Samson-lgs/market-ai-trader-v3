63ebffaedf3b6c8639f4dae0925f6bfc1a66f9eb
Samson Jose J <samsonjosej.2023cncim315@atmemys.onmicrosoft.com>
2026-09-13 21:55:16 +0530

    Export professional analysis modules

--- src/analysis/index.js
+++ src/analysis/index.js
@@ -3,3 +3,5 @@ export { ema, sma, rsi, atr, macd, trueRange } from './indicators.js';
 export { pivots, marketStructure, nearestLevels } from './structure.js';
 export { equalHighLowZones, liquiditySweep, displacement } from './liquidity.js';
 export { scoreCandidate } from './decision.js';
+export { sessionContext, sessionRisk, setupQuality, riskGate } from './professional.js';
+export { normalizeNewsRisk, eventRiskGate } from './events.js';
