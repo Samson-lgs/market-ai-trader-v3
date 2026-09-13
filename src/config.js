b6bf345cfcd2feb23fdcee2df2c69efd52013e9a
Samson Jose J <samsonjosej.2023cncim315@atmemys.onmicrosoft.com>
2026-09-13 23:06:19 +0530

    Add runtime realtime backend configuration

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const MARKET_API_BASE_URL = globalThis.MARKET_API_BASE_URL || '';
+export const CALENDAR_API_BASE_URL = globalThis.CALENDAR_API_BASE_URL || MARKET_API_BASE_URL;
+export const REALTIME_WS_URL = globalThis.MARKET_AI_REALTIME_WS_URL || '';
